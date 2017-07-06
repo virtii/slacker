1159121f480fbd72d0f7c143ca8499d54bae570f
Petr Križek <krizepet@fel.cvut.cz>
2017-07-06 19:16:59 +0200

    initial set up

---
+++ example.js
@@ -0,0 +1,17 @@
+process.env = Object.assign(process.env, {
+  "NODE_ENV": "production",
+  "PORT": "5022",
+  "APP_URL": "https://blabla.virtii.com",
+  "SLACK_APP_NAME": "blabla-dev",
+  "SLACK_WEBHOOK": "XXXXXX"
+})
+
+import Slacker from './src/app.js'
+
+const sl =  new Slacker()
+
+// custom text
+// sl.notify('texttt  \n new line *bold*')
+
+// app was started and server is listening!
+sl.started()
---
+++ src/app.js
@@ -0,0 +1,33 @@
+import request from 'request'
+
+export default class Slacker {
+  constructor(){
+    this.url = process.env.SLACK_WEBHOOK
+    this.appName = process.env.SLACK_APP_NAME
+    this.port = process.env.PORT
+    this.appUrl = process.env.APP_URL
+    this.isProduction = process.env.NODE_ENV === 'production'
+  }
+
+  notify(text) {
+    if (!this.isProduction) return
+    request.post(this.url, {
+         form: {
+           payload: JSON.stringify({text})
+         }
+       }, (err, response) => {
+         if (err) {
+           console.log("err", err, response)
+         }
+       });
+  }
+
+  started() {
+    this.notify(`
+      The project *${this.appName}* was started!
+      URL: ${this.appUrl}
+      PORT: ${this.port}
+      `
+    )
+  }
+}
